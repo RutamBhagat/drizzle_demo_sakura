2d5105e6b8fe50b6e5a44693848561e16fcdd820
RutamBhagat <rutambhagat@gmail.com>
2023-10-22 01:07:55 +0530

    SQL types

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,43 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import {
+  bigint,
+  bigserial,
+  boolean,
+  char,
+  date,
+  decimal,
+  doublePrecision,
+  interval,
+  json,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   fullName: text("full_name"),
   phone: varchar("phone", { length: 256 }),
+  address: varchar("address", { length: 256 }),
+});
+
+export const moodEnum = pgEnum("moodEnum", ["sad", "ok", "happy"]);
+
+export const testTable = pgTable("testTable", {
+  id: bigserial("id", { mode: "bigint" }).primaryKey(),
+  qty: bigint("qty", { mode: "bigint" }),
+  price: decimal("price", { precision: 7, scale: 2 }),
+  score: doublePrecision("score"),
+  delivered: boolean("delivered"),
+  article: text("article"),
+  description: varchar("description", { length: 256 }),
+  name: char("name", { length: 10 }),
+  data: json("data").notNull(),
+  startAt: timestamp("start_at", { precision: 0, withTimezone: false }).defaultNow(),
+  time: timestamp("time", { mode: "date" }).defaultNow(),
+  createdAtDate: date("created_at_date", { mode: "date" }).defaultNow(),
+  timeInterval: interval("time_interval"),
+  mood: moodEnum("mood").default("ok"),
 });
